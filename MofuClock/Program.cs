2ec2415264102ccbb08c86725c0549c938bbce9a
usausa <machi.pon@gmail.com>
2022-11-02 17:37:57 +0900

    Add project

---
+++ MofuClock/Program.cs
@@ -0,0 +1,10 @@
+using MofuClock;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
