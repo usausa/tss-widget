a9ecd29a883abf27d91c8f754a9ef8378dd578f3
usausa <machi.pon@gmail.com>
2026-08-31 08:19:06 +0900

    Update

--- MofuClock/GlobalSuppressions.cs
+++ MofuClock/GlobalSuppressions.cs
@@ -1,1 +1,3 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1848:Use the LoggerMessage delegates", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1873:Avoid potentially expensive logging", Justification = "Ignore")]
